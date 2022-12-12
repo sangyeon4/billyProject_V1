28d2205bb1e2d8c607a9d9594d0f204c195d283e
jinhokim7 <kimjin737373@gmail.com>
2022-12-12 09:49:12 +0900

    빌리 좋아요기능추가, 빌리보기에서 달력수정

--- billy2022-12-06/src/main/java/com/billy/Service/IF_billyService.java
+++ billy2022-12-06/src/main/java/com/billy/Service/IF_billyService.java
@@ -3,12 +3,17 @@ package com.billy.Service;
 import java.util.List;
 
 import com.billy.VO.BillyGoodsVO;
+import com.billy.VO.BillyGoods_likeVO;
 
 public interface IF_billyService {
 	public void insertBilly(BillyGoodsVO bvo) throws Exception;
 
 	public int selectMaxCnt() throws Exception;
-	public List<BillyGoodsVO> selectBillyAll();
-	public BillyGoodsVO selectBillyOne(String vno);
+	public List<BillyGoodsVO> selectBillyAll()throws Exception;
+	public BillyGoodsVO selectBillyOne(String vno) throws Exception;	
 	public void updateBilly(BillyGoodsVO bvo) throws Exception;
+	public void updateBillyMemberId(String id) throws Exception; //탈퇴 시 처리
+	public int selectCntBilly_like(BillyGoods_likeVO blvo) throws Exception;  //좋아요파악
+	public void billyLikeUp(BillyGoods_likeVO blvo) throws Exception; //좋아요누르기
+	public void billyLikeDown(BillyGoods_likeVO blvo) throws Exception; //좋아요취소
 }
